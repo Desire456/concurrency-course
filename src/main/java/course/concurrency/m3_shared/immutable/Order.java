6734df77733bca897ac69482c0cae66e7de4fa06
Vladislav Selitskii <selickiy.vladislav@gmail.com>
2023-04-06 23:30:26 +0400

    Refactored OrderService

--- src/main/java/course/concurrency/m3_shared/immutable/Order.java
+++ src/main/java/course/concurrency/m3_shared/immutable/Order.java
@@ -2,24 +2,32 @@ package course.concurrency.m3_shared.immutable;
 
 import java.util.List;
 
+import static course.concurrency.m3_shared.immutable.Order.Status.IN_PROGRESS;
 import static course.concurrency.m3_shared.immutable.Order.Status.NEW;
 
 public class Order {
 
-    public enum Status { NEW, IN_PROGRESS, DELIVERED }
+    public enum Status {NEW, IN_PROGRESS, DELIVERED}
 
-    private Long id;
-    private List<Item> items;
-    private PaymentInfo paymentInfo;
-    private boolean isPacked;
-    private Status status;
+    private final Long id;
+    private final List<Item> items;
+    private final PaymentInfo paymentInfo;
+    private final boolean isPacked;
+    private final Status status;
 
-    public Order(List<Item> items) {
-        this.items = items;
-        this.status = NEW;
+    public Order(Long id, List<Item> items) {
+        this(id, items, null, false, NEW);
     }
 
-    public synchronized boolean checkStatus() {
+    public Order(Long id, List<Item> items, PaymentInfo paymentInfo, boolean isPacked, Status status) {
+        this.id = id;
+        this.items = items;
+        this.paymentInfo = paymentInfo;
+        this.isPacked = isPacked;
+        this.status = status;
+    }
+
+    public boolean checkStatus() {
         if (items != null && !items.isEmpty() && paymentInfo != null && isPacked) {
             return true;
         }
@@ -30,10 +38,6 @@ public class Order {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public List<Item> getItems() {
         return items;
     }
@@ -42,25 +46,23 @@ public class Order {
         return paymentInfo;
     }
 
-    public void setPaymentInfo(PaymentInfo paymentInfo) {
-        this.paymentInfo = paymentInfo;
-        this.status = Status.IN_PROGRESS;
+    public Order withPaymentInfo(PaymentInfo paymentInfo) {
+        return new Order(id, items, paymentInfo, isPacked, IN_PROGRESS);
     }
 
     public boolean isPacked() {
         return isPacked;
     }
 
-    public void setPacked(boolean packed) {
-        isPacked = packed;
-        this.status = Status.IN_PROGRESS;
+    public Order withPacked(boolean packed) {
+        return new Order(id, items, paymentInfo, packed, IN_PROGRESS);
     }
 
     public Status getStatus() {
         return status;
     }
 
-    public void setStatus(Status status) {
-        this.status = status;
+    public Order withStatus(Status status) {
+        return new Order(id, items, paymentInfo, isPacked, status);
     }
 }
--- src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
+++ src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
@@ -1,46 +1,42 @@
 package course.concurrency.m3_shared.immutable;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class OrderService {
 
-    private Map<Long, Order> currentOrders = new HashMap<>();
+    private final Map<Long, Order> currentOrders = new ConcurrentHashMap<>();
     private long nextId = 0L;
 
     private synchronized long nextId() {
         return nextId++;
     }
 
-    public synchronized long createOrder(List<Item> items) {
+    public long createOrder(List<Item> items) {
         long id = nextId();
-        Order order = new Order(items);
-        order.setId(id);
+        var order = new Order(id, items);
         currentOrders.put(id, order);
         return id;
     }
 
-    public synchronized void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
-        currentOrders.get(orderId).setPaymentInfo(paymentInfo);
-        if (currentOrders.get(orderId).checkStatus()) {
-            deliver(currentOrders.get(orderId));
+    public void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
+        currentOrders.computeIfPresent(orderId, (k, v) -> v.withPaymentInfo(paymentInfo));
+        deliverIfNecessary(currentOrders.get(orderId));
+    }
+
+    public void setPacked(long orderId) {
+        currentOrders.computeIfPresent(orderId, (k, v) -> v.withPacked(true));
+        deliverIfNecessary(currentOrders.get(orderId));
+    }
+
+    private synchronized void deliverIfNecessary(Order order) {
+        if (order.checkStatus()) {
+            currentOrders.computeIfPresent(order.getId(), (k, v) -> v.withStatus(Order.Status.DELIVERED));
         }
     }
 
-    public synchronized void setPacked(long orderId) {
-        currentOrders.get(orderId).setPacked(true);
-        if (currentOrders.get(orderId).checkStatus()) {
-            deliver(currentOrders.get(orderId));
-        }
-    }
-
-    private synchronized void deliver(Order order) {
-        /* ... */
-        currentOrders.get(order.getId()).setStatus(Order.Status.DELIVERED);
-    }
-
-    public synchronized boolean isDelivered(long orderId) {
+    public boolean isDelivered(long orderId) {
         return currentOrders.get(orderId).getStatus().equals(Order.Status.DELIVERED);
     }
 }
